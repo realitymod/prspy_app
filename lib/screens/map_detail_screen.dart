f1c0cca3d5f84df58ef6b4b1eb7220b132357219
Álvaro Claro dos Anjos <alvarocda@gmail.com>
2022-05-31 14:01:38 -0400

    Changed tabbar indicator color of map_detail_screen.dart to match tabbar indicator color of server_list_screen.dart

--- lib/screens/map_detail_screen.dart
+++ lib/screens/map_detail_screen.dart
@@ -86,6 +86,7 @@ class _MapDetailScreenState extends State<MapDetailScreen> {
                 _viewInMapGallery(),
               ],
               bottom: TabBar(
+                indicatorColor: Colors.white,
                 tabs: <Tab>[
                   Tab(
                     child: Column(
